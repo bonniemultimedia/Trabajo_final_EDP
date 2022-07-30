339be90567ef93347447b9b3ee3747520bd0a0bc
Bonnie <bonnie.multimedia@gmail.com>
2022-07-29 22:50:49 -0300

    Script3 Final

--- findNames.sh
+++ findNames.sh
@@ -2,15 +2,24 @@
 
 regExpScript3="^[A-Z]"
 
-wordVal="[0-9]"
+wordVal="^[a-zA-Z]*$"
 
 read -p "Ingrese una palabra: " word
 
-if [[ $word =~ $regExpScript3 ]]
-then
+oneWord=$(echo "$word" | wc -w)
+
+[ $oneWord -ne 1 ] && echo "Debe ingresar una palabra" && exit
+
+if [[ $word =~ $wordVal ]]
+then 
+     if [[ $word =~ $regExpScript3 ]]
+     then
 	echo "Esta palabra es un Nombre Propio"
-else 
+     else 
 	echo "No es Nombre Propio"
+     fi
+else
+     echo "No es una palabra"
 fi
 
 
