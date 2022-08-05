737c38b7203cbe88a46fe8afcfe1e57f18f99f30
Bonnie <bonnie.multimedia@gmail.com>
2022-08-05 14:58:21 -0300

    Script statsWords - FINAL

--- statsWords.sh
+++ statsWords.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-#SCRIPT SIN TERMINAR - FALTA SACAR EL PROMEDIO Y VALIUDAR LAS PALABRAS DE MISMA LONGITUD.
-
-#Lectura de texto
+#Lectura de texto ingresado por teclado
 read -p "Ingrese un texto: " text
 
 #Creo un archivo
 touch texts/script1
 
-echo "" > texts/script1
-
 #Busco espacios y los reemplazo por un salto de línea para separar las palabras
 
 words=(${text// / })
 
-#Creo las variables dode voy a guardar la palabra más larga, la más corta y el promedio.
+#Creo las variables donde voy a guardar la palabra más larga, la más corta y el promedio. Las inicializo en 0.
 
 word1=$(echo "")
 
@@ -22,11 +18,11 @@ longestW=$(echo -n $word1 | wc -c)
 
 shortestW=$(echo -n  $word1 | wc -c)
 
-#Recorro el array y guardo una palabra por línea
+#Recorro el array e imprimo una palabra por línea
 
-#Guardo la cantidad de caracteres que tiene la palabra en la variable wordLength
+#Guardo la cantidad de caracteres que tiene cada palabra en la variable wordLength
 
-#Si es la primera interaccion del for guardo el valor de wordLength en la shortestW para comparar la palabra más corta
+#En cada interacción comparo si la palabra ingresada tiene mayor cantidad de caracteres que los que almacena la variable longestW y guardo el valor en la misma variable. 
 
 for i in "${words[@]}"
 do
@@ -38,6 +34,8 @@ do
               palabralarga=$i
          fi
 done
+
+#Para calcular la palabra más corta, creo una variable que contiene el valor de la palabra con mayor cantidad de caracteres.
 
 shortestW=$longestW
 
@@ -52,8 +50,12 @@ do
           fi
 done
 
+suma=$(expr $shortestW + $longestW)
+prom=$(echo $suma / 2 | bc -l)
+
 echo "La palabra mas larga es $palabralarga y tiene $longestW caracteres"
 echo "La palabra mas corta es $palabracorta y tiene $shortestW caracteres"
+echo "El promedio es: $prom"
 
 rm texts/script1
 
